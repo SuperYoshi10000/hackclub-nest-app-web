dcc0e53d33ae649ca145e8e153bc60d72a73868c
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2026-06-12 21:56:52 -0400

    change database connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,4 @@
+import { POSTGRES_DATABASE, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER } from '$env/static/private';
 import pg from 'pg';
 
 let pool: pg.Pool;
@@ -5,7 +6,11 @@ let client: pg.PoolClient;
 
 export async function init() {
     pool = new pg.Pool({
-        connectionString: process.env.POSTGRES_URL
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+        host: POSTGRES_HOST || 'localhost',
+        port: Number(POSTGRES_PORT || 5432),
+        database: POSTGRES_DATABASE
     });
     client = await pool.connect();
 }
