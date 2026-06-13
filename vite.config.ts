cbaafc2837af4ba394599cbc4f5f0f4519b51fe4
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2026-06-12 21:50:16 -0400

    load env in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [
+			sveltekit({
+				compilerOptions: {
+					// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+					runes: ({ filename }) =>
+						filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+				},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
-		})
-	],
-	server: {
-		allowedHosts: ["localhost", import.meta.env.VITE_HOSTNAME]
+				// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+				// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+				// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+				adapter: adapter()
+			})
+		],
+		server: {
+			allowedHosts: ["localhost", env.VITE_HOSTNAME]
+		}
 	}
 });
